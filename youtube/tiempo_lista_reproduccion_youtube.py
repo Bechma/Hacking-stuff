ceec899356577dc38aaf425e47826c56a772c57b
Bechma <brunochenoll@hotmail.com>
2017-06-02 00:15:11 +0200

    Update tiempo_lista_reproduccion_youtube.py
    Ahora no necesita librerías de terceros y coge bien los tiempos

--- youtube/tiempo_lista_reproduccion_youtube.py
+++ youtube/tiempo_lista_reproduccion_youtube.py
@@ -1,20 +1,19 @@
-import requests
+from urllib.request import urlopen
 import re
 
-url = input("Introduce el playlist a analizar: ")
+url = input("Introduce url del playlist a analizar: ")
 
 try:
-    pagina = requests.get(url).text
+	with urlopen(url) as page:
+		a = re.findall(">(\d+):(\d+)<", page.read().decode("utf-8"))
 
-    a = re.findall("<span aria-label=\"(\d+) minutos, (\d+) segundos\">", pagina)
+	min = sum([int(par[0]) for par in a])
+	sec = sum([int(par[1]) for par in a])
+	min += int(sec / 60)
+	hour = int(min / 60)
+	min %= 60
+	sec %= 60
 
-    min = sum([int(par[0]) for par in a])
-    sec = sum([int(par[1]) for par in a])
-    min += int(sec / 60)
-    hour = int(min / 60)
-    min %= 60
-    sec %= 60
-
-    print("{2} horas {0} minutos {1} segundos".format(min, sec, hour))
+	print("{2} horas {0} minutos {1} segundos".format(min, sec, hour))
 except Exception:
-    print("playlist inválido, por favor, introduce un playlist correcto")
+	print("playlist inválido, por favor, introduce un playlist correcto")
